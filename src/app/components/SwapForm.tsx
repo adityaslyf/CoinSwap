5e6458c57d360bb73d89a0416876d97d18fe6294
adityaslyf <aditya.varshneymail@gmail.com>
2024-06-12 02:51:05 +0530

    added token text

--- src/app/components/SwapForm.tsx
+++ src/app/components/SwapForm.tsx
@@ -1,15 +1,15 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import Web3 from 'web3';
-import BigNumber from 'bignumber.js';
-import qs from 'qs';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './Navbar';  
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+import BigNumber from "bignumber.js";
+import qs from "qs";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Navbar";
 declare global {
-    interface Window{
-      ethereum?: any;
-    }
+  interface Window {
+    ethereum?: any;
   }
+}
 interface Token {
   symbol: string;
   logoURI: string;
@@ -19,8 +19,11 @@ interface Token {
 
 const SwapForm: React.FC = () => {
   const [tokens, setTokens] = useState<Token[]>([]);
-  const [currentTrade, setCurrentTrade] = useState<{ from?: Token; to?: Token }>({});
-  const [currentSelectSide, setCurrentSelectSide] = useState<string>('');
+  const [currentTrade, setCurrentTrade] = useState<{
+    from?: Token;
+    to?: Token;
+  }>({});
+  const [currentSelectSide, setCurrentSelectSide] = useState<string>("");
   const [web3, setWeb3] = useState<Web3 | null>(null);
   const [account, setAccount] = useState<string | null>(null);
 
@@ -32,24 +35,33 @@ const SwapForm: React.FC = () => {
   }, []);
 
   const listAvailableTokens = async () => {
-    const response = await fetch("https://tokens.coingecko.com/uniswap/all.json");
+    const response = await fetch(
+      "https://tokens.coingecko.com/uniswap/all.json"
+    );
     const tokenListJSON = await response.json();
     setTokens(tokenListJSON.tokens);
   };
 
   const selectToken = (token: Token) => {
     closeModal();
-    setCurrentTrade(prevState => ({ ...prevState, [currentSelectSide]: token }));
+    setCurrentTrade((prevState) => ({
+      ...prevState,
+      [currentSelectSide]: token,
+    }));
   };
 
   const renderInterface = () => {
     if (currentTrade.from) {
-      (document.getElementById("from_token_img") as HTMLImageElement).src = currentTrade.from.logoURI;
-      (document.getElementById("from_token_text") as HTMLElement).innerText = currentTrade.from.symbol;
+      (document.getElementById("from_token_img") as HTMLImageElement).src =
+        currentTrade.from.logoURI;
+      (document.getElementById("from_token_text") as HTMLElement).innerText =
+        currentTrade.from.symbol;
     }
     if (currentTrade.to) {
-      (document.getElementById("to_token_img") as HTMLImageElement).src = currentTrade.to.logoURI;
-      (document.getElementById("to_token_text") as HTMLElement).innerText = currentTrade.to.symbol;
+      (document.getElementById("to_token_img") as HTMLImageElement).src =
+        currentTrade.to.logoURI;
+      (document.getElementById("to_token_text") as HTMLElement).innerText =
+        currentTrade.to.symbol;
     }
   };
 
@@ -76,49 +88,79 @@ const SwapForm: React.FC = () => {
         console.error(error);
       }
     } else {
-      document.getElementById("login_button")!.innerText = "Please install MetaMask";
+      document.getElementById("login_button")!.innerText =
+        "Please install MetaMask";
     }
   };
 
   const getPrice = async () => {
-    if (!currentTrade.from || !currentTrade.to || !(document.getElementById("from_amount")! as HTMLInputElement).value) return;
+    if (
+      !currentTrade.from ||
+      !currentTrade.to ||
+      !(document.getElementById("from_amount")! as HTMLInputElement).value
+    )
+      return;
 
-    const amount = Number((document.getElementById("from_amount")! as HTMLInputElement).value) * (10 ** currentTrade.from.decimals);
+    const amount =
+      Number(
+        (document.getElementById("from_amount")! as HTMLInputElement).value
+      ) *
+      10 ** currentTrade.from.decimals;
     const params = {
-        sellToken: currentTrade.from.address,
-        buyToken: currentTrade.to.address,
-        sellAmount: amount,
+      sellToken: currentTrade.from.address,
+      buyToken: currentTrade.to.address,
+      sellAmount: amount,
     };
 
     const headers = {
-        '0x-api-key': process.env.NEXT_PUBLIC_0X_API_KEY as string,
+      "0x-api-key": process.env.NEXT_PUBLIC_0X_API_KEY as string,
     };
 
-    const response = await fetch(`https://api.0x.org/swap/v1/quote?${qs.stringify(params)}`, { headers });
+    const response = await fetch(
+      `https://api.0x.org/swap/v1/quote?${qs.stringify(params)}`,
+      { headers }
+    );
     const swapQuoteJSON = await response.json();
 
-    (document.getElementById("to_amount")! as HTMLInputElement).value = String(swapQuoteJSON.buyAmount / (10 ** currentTrade.to.decimals));
-    (document.getElementById("gas_estimate") as HTMLElement).innerText = swapQuoteJSON.estimatedGas;
+    (document.getElementById("to_amount")! as HTMLInputElement).value = String(
+      swapQuoteJSON.buyAmount / 10 ** currentTrade.to.decimals
+    );
+    (document.getElementById("gas_estimate") as HTMLElement).innerText =
+      swapQuoteJSON.estimatedGas;
 
     return swapQuoteJSON;
   };
 
   const getQuote = async (account: string) => {
-    if (!currentTrade.from || !currentTrade.to || !(document.getElementById("from_amount")! as HTMLInputElement).value) return;
+    if (
+      !currentTrade.from ||
+      !currentTrade.to ||
+      !(document.getElementById("from_amount")! as HTMLInputElement).value
+    )
+      return;
 
-    const amount = Number((document.getElementById("from_amount")! as HTMLInputElement).value) * (10 ** currentTrade.from.decimals);
+    const amount =
+      Number(
+        (document.getElementById("from_amount")! as HTMLInputElement).value
+      ) *
+      10 ** currentTrade.from.decimals;
     const params = {
-        sellToken: currentTrade.from.address,
-        buyToken: currentTrade.to.address,
-        sellAmount: amount,
-        takerAddress: account,
+      sellToken: currentTrade.from.address,
+      buyToken: currentTrade.to.address,
+      sellAmount: amount,
+      takerAddress: account,
     };
 
-    const response = await fetch(`https://api.0x.org/swap/v1/quote?${qs.stringify(params)}`);
+    const response = await fetch(
+      `https://api.0x.org/swap/v1/quote?${qs.stringify(params)}`
+    );
     const swapQuoteJSON = await response.json();
 
-    (document.getElementById("to_amount")! as HTMLInputElement).value = String(swapQuoteJSON.buyAmount / (10 ** currentTrade.to.decimals));
-    (document.getElementById("gas_estimate") as HTMLElement).innerText = swapQuoteJSON.estimatedGas;
+    (document.getElementById("to_amount")! as HTMLInputElement).value = String(
+      swapQuoteJSON.buyAmount / 10 ** currentTrade.to.decimals
+    );
+    (document.getElementById("gas_estimate") as HTMLElement).innerText =
+      swapQuoteJSON.estimatedGas;
 
     return swapQuoteJSON;
   };
@@ -128,57 +170,62 @@ const SwapForm: React.FC = () => {
     const swapQuoteJSON = await getQuote(account);
     const erc20abi = [
       {
-        "constant": true,
-        "inputs": [],
-        "name": "name",
-        "outputs": [
+        constant: true,
+        inputs: [],
+        name: "name",
+        outputs: [
           {
-            "name": "",
-            "type": "string"
-          }
+            name: "",
+            type: "string",
+          },
         ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
+        payable: false,
+        stateMutability: "view",
+        type: "function",
       },
       {
-        "constant": false,
-        "inputs": [
+        constant: false,
+        inputs: [
           {
-            "name": "_spender",
-            "type": "address"
+            name: "_spender",
+            type: "address",
           },
           {
-            "name": "_value",
-            "type": "uint256"
-          }
+            name: "_value",
+            type: "uint256",
+          },
         ],
-        "name": "approve",
-        "outputs": [
+        name: "approve",
+        outputs: [
           {
-            "name": "",
-            "type": "bool"
-          }
+            name: "",
+            type: "bool",
+          },
         ],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
       },
     ];
     const fromTokenAddress = currentTrade.from!.address;
-    const ERC20TokenContract = new web3.eth.Contract(erc20abi, fromTokenAddress);
+    const ERC20TokenContract = new web3.eth.Contract(
+      erc20abi,
+      fromTokenAddress
+    );
 
     const maxApproval = new BigNumber(2).pow(256).minus(1);
 
-    await ERC20TokenContract.methods.approve(swapQuoteJSON.allowanceTarget, maxApproval).send({ from: account });
+    await ERC20TokenContract.methods
+      .approve(swapQuoteJSON.allowanceTarget, maxApproval)
+      .send({ from: account });
 
     const txParams = {
       ...swapQuoteJSON,
       from: account,
-      gas: swapQuoteJSON.estimatedGas
+      gas: swapQuoteJSON.estimatedGas / 10000,
     };
 
-    await web3.eth.sendTransaction(txParams).on('transactionHash', (hash) => {
+    await web3.eth.sendTransaction(txParams).on("transactionHash", (hash) => {
       console.log(`Transaction hash: ${hash}`);
     });
   };
@@ -189,34 +236,60 @@ const SwapForm: React.FC = () => {
 
   return (
     <div className="container">
-        <div className=' bg-gra7'>
+      <div className=" bg-gra7">
         <Navbar onConnect={connectWallet} />
-        </div>
-      
+      </div>
+
       <div className="row">
         <div className="col col-md-6 offset-md-3" id="window">
           <h4>Swap</h4>
           <div id="form">
             <div className="swapbox">
-              <div className="swapbox_select token_select" id="from_token_select" onClick={() => openModal('from')}>
+              <div
+                className="swapbox_select token_select"
+                id="from_token_select"
+                onClick={() => openModal("from")}
+              >
                 <img className="token_img" id="from_token_img" />
-                <span id="from_token_text"></span>
+                <span id="from_token_text">Select a Token</span>
               </div>
               <div className="swapbox_select">
-                <input className="number form-control" placeholder="amount" id="from_amount" onBlur={getPrice} />
+                <input
+                  className="number form-control"
+                  placeholder="amount"
+                  id="from_amount"
+                  onBlur={getPrice}
+                />
               </div>
             </div>
             <div className="swapbox">
-              <div className="swapbox_select token_select" id="to_token_select" onClick={() => openModal('to')}>
+              <div
+                className="swapbox_select token_select"
+                id="to_token_select"
+                onClick={() => openModal("to")}
+              >
                 <img className="token_img" id="to_token_img" />
-                <span id="to_token_text"></span>
+                <span id="to_token_text">Select a Token</span>
               </div>
-              <div className="swapbox_select">
-                <input className="number form-control" placeholder="amount" id="to_amount" />
+              <div className="swapbox_select ">
+                <input
+                  className="number form-control"
+                  placeholder="amount"
+                  id="to_amount"
+                />
               </div>
             </div>
-            <div className="gas_estimate_label">Estimated Gas: <span id="gas_estimate"></span></div>
-            <button disabled className="btn btn-large btn-primary btn-block" id="swap_button" onClick={trySwap}>Swap</button>
+            <div className="gas_estimate_label">
+              Estimated Gas: <span id="gas_estimate"></span>
+            </div>
+            <button
+              disabled
+              className="btn btn-large btn-primary btn-block"
+              id="swap_button"
+              onClick={trySwap}
+            >
+              Swap
+            </button>
           </div>
         </div>
       </div>
@@ -225,13 +298,22 @@ const SwapForm: React.FC = () => {
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title">Select a token</h5>
-              <button type="button" className="close" aria-label="Close" onClick={closeModal}>
+              <button
+                type="button"
+                className="close"
+                aria-label="Close"
+                onClick={closeModal}
+              >
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div className="modal-body">
-              {tokens.map(token => (
-                <div key={token.address} className="token_row" onClick={() => selectToken(token)}>
+              {tokens.map((token) => (
+                <div
+                  key={token.address}
+                  className="token_row"
+                  onClick={() => selectToken(token)}
+                >
                   <img className="token_list_img" src={token.logoURI} />
                   <span className="token_list_text">{token.symbol}</span>
                 </div>
